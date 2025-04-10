ea016a63dfc7e6e33c80df33c9b14e154c8c0b67
ZhangRun <127740919+Water-Run@users.noreply.github.com>
2025-04-10 09:19:08 +0800

    初始项目提交

---
+++ README.md
@@ -0,0 +1,15 @@
+# pdf-drawing-ocr-recognition  
+
+一个开源的定制项目,一个Python库,实现将PDF输入的指定模式的表格进行数据化读取.  
+
+## 安装  
+
+项目已经发布在PyPi上,使用`pip`进行安装:  
+
+```cmd
+pip install pdor
+```
+
+库的名称简写为`pdor`.  
+
+## 使用教程  
---
+++ pdf_drawing_ocr_recognition/pdor_unit.py
@@ -0,0 +1,21 @@
+r"""
+PDR单元
+:author: WaterRun
+:time: 2025-04-10
+:file: pdr_unit.py
+"""
+
+import simpsave as ss
+
+
+class Unit:
+
+    r"""
+
+    """
+
+    def __init__(self, file: str):
+        ...
+
+    def output(self, file_name: str) -> bool:
+        ...
